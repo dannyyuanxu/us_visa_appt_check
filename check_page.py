123ab2a463a8c3088c6a2a478d54e21fcdd4d3bf
Yuan Xu <dannyyuanxu@gmail.com>
2021-10-16 00:31:49 -0400

    find next available date and click - debugging

--- check_page.py
+++ check_page.py
@@ -3,7 +3,11 @@
 #### set up
 import yaml
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait as wait
+from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
+
 import os
 import dotenv
 
@@ -19,8 +23,6 @@ login_site = config["reschedule_web"]
 dotenv.load_dotenv()
 login_username = os.getenv('USERNAME')
 login_password = os.getenv('PASSWORD')
-
-
 
 
 #### webpage operations
@@ -47,7 +49,42 @@ driver.execute_script("arguments[0].click();", policy_check)
 sign_in_button = driver.find_element_by_name("commit")
 sign_in_button.click()
 
-# select
+# test out one city
+
+city_list = ['Ottawa','Calgary','Vancouver','Halifax','Montreal','Quebec City','Toronto']
+
+city_field = driver.find_element_by_name("appointments[consulate_appointment][facility_id]")
+city_field.send_keys("Ottawa")
+
+
+avail_field = driver.find_element_by_id("appointments_consulate_appointment_date_input")
+avail_field.click()
+
+
+
+#### under development
+
+while True:
+    try:
+        print("start trying to click")
+        wait(driver, 2).until(EC.visibility_of_element_located((By.XPATH, "//td[@data-handler='selectDay']"))).click()
+    except: 
+        print("move to next page")
+        driver.find_element(By.XPATH,  "//div[@class='ui-datepicker-group ui-datepicker-group-last']//div[@class='ui-datepicker-header ui-widget-header ui-helper-clearfix ui-corner-right']//a[@class='ui-datepicker-next ui-corner-all']").click()
+
+# date_list = driver.find_elements(By.XPATH, '//table[@td class=" undefined"]')
+
+# if len(date_list)>0:
+#     date_list[0].click()
+
+# date_list = driver.find_elements(By.XPATH, '//table[@td class=" undefined"]')
+        
+
+
+
+
+
+
 
 
 
